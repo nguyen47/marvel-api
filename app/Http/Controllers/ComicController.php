b72bcce022e1628bcd478cb4439966de2bcc15c3
Nguyen Hoang <hoangtrankhoinguyen@gmail.com>
2018-06-03 18:50:26 +0700

    trash

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -17,7 +17,6 @@ class ComicController extends Controller
 				'hash' => $hash
 			];
 			$url = "https://gateway.marvel.com:443/v1/public/characters?name=".$characters.'&'.http_build_query($params);
-			dd($url);
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -25,6 +24,7 @@ class ComicController extends Controller
 			$result=curl_exec($ch);
 			curl_close($ch);
 			$result = json_decode($result, true);
+			dd($result);
 			if (isset($result['data']['results'])) {
 				$data = $result['data']['results'][0];
 				$arrayComics = $result['data']['results'][0]['comics']['items'];
